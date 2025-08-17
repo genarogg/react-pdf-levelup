1f7f85a046f65ebb7b2dbf9fb1a557423932b30b
Genaro Gonzalez <gena_gonzalez15@hotmail.com>
2025-08-17 14:54:42 -0400

    WIP on master

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useRef, useEffect } from "react"
 import { Editor } from "@monaco-editor/react"
-import { useRef, useEffect } from "react"
 
 interface CodeEditorProps {
   value: string
@@ -36,74 +35,74 @@ const CodeEditor = ({ value, onChange }: CodeEditorProps) => {
       // Componentes de layout
       {
         label: "LayoutPDF",
-        insertText: '<LayoutPDF size="A4" padding={30} showPageNumbers={true}>\n  \n</LayoutPDF>',
+        insertText: '<LayoutPDF size="A4" orientation="v" showPageNumbers={true}>\n\n</LayoutPDF>',
         kind: monaco.languages.CompletionItemKind.Snippet,
       },
       {
         label: "Container",
-        insertText: "<Container>\n  \n</Container>",
+        insertText: "<Container>  </Container>",
         kind: monaco.languages.CompletionItemKind.Snippet,
       },
-      { label: "Row", insertText: "<Row>\n  \n</Row>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Row", insertText: "<Row></Row>", kind: monaco.languages.CompletionItemKind.Snippet },
 
       // Componentes de columnas
-      { label: "Col1", insertText: "<Col1>\n  \n</Col1>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Col2", insertText: "<Col2>\n  \n</Col2>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Col3", insertText: "<Col3>\n  \n</Col3>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Col4", insertText: "<Col4>\n  \n</Col4>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Col5", insertText: "<Col5>\n  \n</Col5>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Col6", insertText: "<Col6>\n  \n</Col6>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Col7", insertText: "<Col7>\n  \n</Col7>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Col8", insertText: "<Col8>\n  \n</Col8>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Col9", insertText: "<Col9>\n  \n</Col9>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Col10", insertText: "<Col10>\n  \n</Col10>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Col11", insertText: "<Col11>\n  \n</Col11>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Col12", insertText: "<Col12>\n  \n</Col12>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Col1", insertText: "<Col1>  </Col1>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Col2", insertText: "<Col2>  </Col2>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Col3", insertText: "<Col3>  </Col3>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Col4", insertText: "<Col4>  </Col4>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Col5", insertText: "<Col5>  </Col5>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Col6", insertText: "<Col6>  </Col6>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Col7", insertText: "<Col7>  </Col7>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Col8", insertText: "<Col8>  </Col8>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Col9", insertText: "<Col9>  </Col9>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Col10", insertText: "<Col10>  </Col10>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Col11", insertText: "<Col11>  </Col11>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Col12", insertText: "<Col12>  </Col12>", kind: monaco.languages.CompletionItemKind.Snippet },
 
       // Componentes de texto
-      { label: "P", insertText: "<P>\n  \n</P>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "H1", insertText: "<H1>\n  \n</H1>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "H2", insertText: "<H2>\n  \n</H2>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "H3", insertText: "<H3>\n  \n</H3>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "H4", insertText: "<H4>\n  \n</H4>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "H5", insertText: "<H5>\n  \n</H5>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "H6", insertText: "<H6>\n  \n</H6>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Strong", insertText: "<Strong>\n  \n</Strong>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Em", insertText: "<Em>\n  \n</Em>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "U", insertText: "<U>\n  \n</U>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Small", insertText: "<Small>\n  \n</Small>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "P", insertText: "<P>  </P>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "H1", insertText: "<H1>  </H1>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "H2", insertText: "<H2>  </H2>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "H3", insertText: "<H3>  </H3>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "H4", insertText: "<H4>  </H4>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "H5", insertText: "<H5>  </H5>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "H6", insertText: "<H6>  </H6>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Strong", insertText: "<Strong>  </Strong>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Em", insertText: "<Em>  </Em>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "U", insertText: "<U>  </U>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Small", insertText: "<Small>  </Small>", kind: monaco.languages.CompletionItemKind.Snippet },
       {
         label: "Blockquote",
-        insertText: "<Blockquote>\n  \n</Blockquote>",
+        insertText: "<Blockquote>  </Blockquote>",
         kind: monaco.languages.CompletionItemKind.Snippet,
       },
-      { label: "Mark", insertText: "<Mark>\n  \n</Mark>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Span", insertText: "<Span>\n  \n</Span>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Mark", insertText: "<Mark>  </Mark>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Span", insertText: "<Span>  </Span>", kind: monaco.languages.CompletionItemKind.Snippet },
       { label: "BR", insertText: "<BR />", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "A", insertText: '<A href="">\n  \n</A>', kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "A", insertText: '<A href="">  </A>', kind: monaco.languages.CompletionItemKind.Snippet },
 
       // Componentes de posicionamiento
-      { label: "Left", insertText: "<Left>\n  \n</Left>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Right", insertText: "<Right>\n  \n</Right>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Center", insertText: "<Center>\n  \n</Center>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Left", insertText: "<Left>  </Left>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Right", insertText: "<Right>  </Right>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Center", insertText: "<Center>  </Center>", kind: monaco.languages.CompletionItemKind.Snippet },
 
       // Componentes de tabla
       {
         label: "Table",
         insertText:
-          "<Table>\n  <Thead>\n    <Tr>\n      <Th></Th>\n    </Tr>\n  </Thead>\n  <Tbody>\n    <Tr>\n      <Td></Td>\n    </Tr>\n  </Tbody>\n</Table>",
+          "<Table>  <Thead>    <Tr>      <Th></Th>    </Tr>  </Thead>  <Tbody>    <Tr>      <Td></Td>    </Tr>  </Tbody></Table>",
         kind: monaco.languages.CompletionItemKind.Snippet,
       },
-      { label: "Thead", insertText: "<Thead>\n  \n</Thead>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Tbody", insertText: "<Tbody>\n  \n</Tbody>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Tr", insertText: "<Tr>\n  \n</Tr>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Th", insertText: "<Th>\n  \n</Th>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Td", insertText: "<Td>\n  \n</Td>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Thead", insertText: "<Thead>  </Thead>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Tbody", insertText: "<Tbody>  </Tbody>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Tr", insertText: "<Tr>  </Tr>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Th", insertText: "<Th>  </Th>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Td", insertText: "<Td>  </Td>", kind: monaco.languages.CompletionItemKind.Snippet },
 
       // Componentes de lista
-      { label: "UL", insertText: "<UL>\n  <LI></LI>\n</UL>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "OL", insertText: "<OL>\n  <LI></LI>\n</OL>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "LI", insertText: "<LI>\n  \n</LI>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "UL", insertText: "<UL>  <LI></LI></UL>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "OL", insertText: "<OL>  <LI></LI></OL>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "LI", insertText: "<LI>  </LI>", kind: monaco.languages.CompletionItemKind.Snippet },
 
       // Otros componentes
       {
@@ -125,8 +124,8 @@ const CodeEditor = ({ value, onChange }: CodeEditorProps) => {
         />`,
         kind: monaco.languages.CompletionItemKind.Snippet,
       },
-      { label: "Header", insertText: "<Header>\n  \n</Header>", kind: monaco.languages.CompletionItemKind.Snippet },
-      { label: "Footer", insertText: "<Footer>\n  \n</Footer>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Header", insertText: "<Header>  </Header>", kind: monaco.languages.CompletionItemKind.Snippet },
+      { label: "Footer", insertText: "<Footer>  </Footer>", kind: monaco.languages.CompletionItemKind.Snippet },
 
       // Snippets para estilos
       {
