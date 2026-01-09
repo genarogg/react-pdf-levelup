ad88f3e0eafc4b588e76714d58ed8e3bf3836e6e
Genaro Gonzalez <gena_gonzalez15@hotmail.com>
2026-01-08 23:52:45 -0400

    refactorizando el back

--- api/src/routers/index.ts
+++ api/src/routers/index.ts
@@ -1,7 +1,13 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
 import { generatePdfController } from '../controllers/index';
 
+
+const controller = (request: FastifyRequest, reply: FastifyReply) => {
+    return reply.send({ message: "Hello from the API!" });
+}
+
 const router = async (fastify: FastifyInstance) => {
+    fastify.get('/', controller);
     fastify.post('/', generatePdfController);
 }
 
