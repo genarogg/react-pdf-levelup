34af1c95314c47b563e83b8ad77ff7c50e640508
Genaro Gonzalez <gena_gonzalez15@hotmail.com>
2026-02-11 03:35:17 -0400

    demo

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,52 +4,21 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/docs',
-	redirects: {
-		'/': '/docs/get-started',
-	},
-	server: {
-		host: '0.0.0.0',
-		allowedHosts: true,
-
-	},
-
 	integrations: [
 		starlight({
-			title: 'react pdf levelup',
-			social: [{
-				icon: 'github',
-				label: 'GitHub',
-				href: 'https://github.com/genarogg/react-pdf-levelup'
-			}],
-			disable404Route: true,
+			title: 'My Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Inicio',
+					label: 'Guides',
 					items: [
-						{ label: 'Get Started', slug: 'get-started' },
-						{ label: 'Playground', slug: 'playground' },
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
 					],
 				},
 				{
-					label: 'Guías',
-					items: [
-						{ label: 'Tu Primer Template', slug: 'guides/first-template' },
-						{ label: 'Integración Backend', slug: 'guides/backend-integration' },
-						{ label: 'API REST (fetch)', slug: 'guides/api-rest' },
-					],
-				},
-				{
-					label: 'Componentes',
-					autogenerate: { directory: 'components' },
-				},
-				{
-					label: 'Funciones',
-					autogenerate: { directory: 'functions' },
-				},
-				{
-					label: 'Plugins',
-					autogenerate: { directory: 'plugin' },
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
 				},
 			],
 		}),
