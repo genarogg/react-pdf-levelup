7dc5e30b0bdb3caeeb322e5bc5c3f2df8f2efbc7
Genaro Gonzalez <genarrogg@gmail.com>
2026-02-20 13:29:49 -0400

    documentacion fix

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -22,10 +22,9 @@ export default defineConfig({
 				})
 			],
 			title: 'REACT PDF LEVELUP',
-			 favicon: './public/iconos/favicon-32x32.png',
+			favicon: './public/iconos/favicon-32x32.png',
 			logo: {
 				src: './public/iconos/favicon-192x192.png',
-				// replacesTitle: true,
 			},
 			customCss: [
 				'./src/styles/custom.css',
@@ -41,51 +40,63 @@ export default defineConfig({
 				es: { label: 'Español', lang: 'es-ES' },
 				en: { label: 'English', lang: 'en-US' },
 			},
-			sidebar: [
-				{
-					label: 'Inicio',
-					translations: {
-						en: 'Start'
-					},
-					items: [
-						{ slug: 'get-started' },
+			// sidebar: [
+			// 	{
+			// 		label: 'Inicio',
+			// 		translations: {
+			// 			en: 'Start'
+			// 		},
+			// 		items: [
+			// 			{ slug: 'get-started' },
+			// 			{ slug: 'playground' },
+			// 		],
+			// 	},
+			// ],
+			// sidebar: [
+			// 	{
+			// 		label: 'Inicio',
+			// 		translations: {
+			// 			en: 'Start'
+			// 		},
+			// 		items: [
+			// 			{ slug: 'get-started' },
 
-						{ slug: 'playground' },
-					],
-				},
-				{
-					label: 'Guías',
-					translations: {
-						en: 'Guides'
-					},
-					items: [
-						{ slug: 'guides/first-template' },
-						{ slug: 'guides/backend-integration' },
-						{ slug: 'guides/api-rest' },
-					],
-				},
-				{
-					label: 'Componentes',
-					translations: {
-						en: 'Components'
-					},
-					autogenerate: { directory: 'components' },
-				},
-				{
-					label: 'Funciones',
-					translations: {
-						en: 'Functions'
-					},
-					autogenerate: { directory: 'functions' },
-				},
-				{
-					label: 'Plugins',
-					translations: {
-						en: 'Plugins'
-					},
-					autogenerate: { directory: 'plugin' },
-				},
-			],
+			// 			{ slug: 'playground' },
+			// 		],
+			// 	},
+			// 	{
+			// 		label: 'Guías',
+			// 		translations: {
+			// 			en: 'Guides'
+			// 		},
+			// 		items: [
+			// 			{ slug: 'guides/first-template' },
+			// 			{ slug: 'guides/backend-integration' },
+			// 			{ slug: 'guides/api-rest' },
+			// 		],
+			// 	},
+			// 	{
+			// 		label: 'Componentes',
+			// 		translations: {
+			// 			en: 'Components'
+			// 		},
+			// 		autogenerate: { directory: 'components' },
+			// 	},
+			// 	{
+			// 		label: 'Funciones',
+			// 		translations: {
+			// 			en: 'Functions'
+			// 		},
+			// 		autogenerate: { directory: 'functions' },
+			// 	},
+			// 	{
+			// 		label: 'Plugins',
+			// 		translations: {
+			// 			en: 'Plugins'
+			// 		},
+			// 		autogenerate: { directory: 'plugin' },
+			// 	},
+			// ],
 		}),
 	],
 });
